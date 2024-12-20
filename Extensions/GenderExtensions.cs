5185f13a0f97c17c55b5faef7cc6c1757fdc63bc
Dreamless2 <tiagonet50@gmail.com>
2024-12-19 22:44:33 -0300

    Updates

---
+++ Extensions/GenderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace Aniflix.Extensions
+{
+    public static class GenderExtensions
+    {
+        public static void CleanNames()
+        {
+            string[] genres = { "ação", "mistério", "ficção científica", "thriller" };
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (var genre in genres)
+            {
+                string normalizedGenre = StringExtensions.RemoveDiacritics(genre.ToLower());
+                string originalGenre = genre.ToLower();
+
+                result.Append($"#{originalGenre.Replace(" ", "")} ");
+
+                if (originalGenre == "ação" || originalGenre == "mistério" || originalGenre == "ficção científica")
+                {
+                    result.Append($"#{normalizedGenre.Replace(" ", "")} ");
+                }
+            }
+            return result.ToString().TrimEnd();
+        }
+    }
+}
