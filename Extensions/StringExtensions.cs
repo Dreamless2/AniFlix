061aaf32e4673bf19beecfd23b1bb04ec60bf47d
Dreamless2 <tiagonet50@gmail.com>
2024-12-19 21:20:08 -0300

    Update StringExtensions.cs

--- Extensions/StringExtensions.cs
+++ Extensions/StringExtensions.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Globalization;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Aniflix.Extensions
 {
-    internal class StringExtensions
+    public static class StringExtensions
     {
+        public static string RemoveDiacritics(this string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder(normalizedString.Length);
+
+            foreach (char c in normalizedString)
+            {
+                if (CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
     }
 }
+
